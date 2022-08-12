06893a60bb621808c128ed9a287bc218f4dc8284
unknown <jc.vargas2150@gmail.com>
2022-08-11 19:41:03 -0500

    requisitos heroku 4

--- TERI/TERI/settings.py
+++ TERI/TERI/settings.py
@@ -136,4 +136,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
