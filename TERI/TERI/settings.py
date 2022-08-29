8cf47b45e1a183f614ae5b0ea445634b58b89b48
unknown <jc.vargas2150@gmail.com>
2022-08-29 16:55:56 -0500

    arreglo para heroku

--- TERI/TERI/settings.py
+++ TERI/TERI/settings.py
@@ -152,7 +152,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
